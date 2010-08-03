351d525e1a2c34dc3eabc8581558a4c38ebdce81
storrgie <root@agdunn.net>
2010-08-03 07:13:43 -0400

    What is this load, must ask Jeremy later.

--- aDA/util/load.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NEW_SCENARIO=$DXC_HOME/Doc/sample/Exp_"$1"_pb_ADAPT-Lite.scn
-
-rm -f $DXC_HOME/Scenarios/Results/*
-rm -f $DXC_HOME/Scenarios/ADAPT-Lite/*
-
-cp $NEW_SCENARIO $DXC_HOME/Scenarios/ADAPT-Lite
