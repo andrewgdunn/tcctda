eaef00b92a29b00b84fbae18bd03569cebd669fd
jmange <jeremy.mange@gmail.com>
2010-08-11 12:40:12 -0400

    added rules for resistence errors

--- jDA/compile.sh
+++ jDA/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Compiling files..."
-javac -cp $DXC_HOME/Src/APIs/java/src MyDA2.java Sensor.java ErrorFinder.java
+javac -cp $DXC_HOME/Src/APIs/java/src MyDA2.java Sensor.java ErrorFinder.java ComponentError.java
 echo "done."
 
 echo -e "\nPacking files..."
