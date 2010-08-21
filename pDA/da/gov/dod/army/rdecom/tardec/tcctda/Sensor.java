423edd690cbbc57d8afb4a45320a4959c6544355
storrgie <root@agdunn.net>
2010-08-21 15:05:58 -0400

    Cleaned up Sensor class

--- pDA/da/gov/dod/army/rdecom/tardec/tcctda/Sensor.java
+++ pDA/da/gov/dod/army/rdecom/tardec/tcctda/Sensor.java
@@ -1,4 +1,5 @@
 package gov.dod.army.rdecom.tardec.tcctda;
+
 import org.dxc.api.datatypes.*;
 
 import java.util.Vector;
@@ -9,7 +10,9 @@ public class Sensor {
 	public Vector<Long> timestamps;
 	public boolean hitsZero;
 	
-	//---------------------------------------------------------------------------------------------
+	private double noiseFloor = 0.3;
+	private int largePositiveNumber = 99999999;
+	private int largeNegativeNumber = -99999999;
 	
 	public Sensor(String sensorID) {
 		id = sensorID;
@@ -18,8 +21,6 @@ public class Sensor {
 		hitsZero = false;
 	}
 	
-	//---------------------------------------------------------------------------------------------
-	
 	public void addData(Value value, long timestamp) {
 		// make all Integers into Reals
 		if(value instanceof IntegerValue)
@@ -27,103 +28,81 @@ public class Sensor {
 		data.add(value);
 		timestamps.add(timestamp);
 		
-		if( (value instanceof IntegerValue || value instanceof RealValue) && ((RealValue)value).get()<0.3 )
+		// In some scenarios the noise will make it so that zero is never truely achieved, noiseFloor is our allowable range
+		if( (value instanceof IntegerValue || value instanceof RealValue) && ((RealValue)value).get() < noiseFloor ) {
 			hitsZero = true;
+		}
+			
 	}
-	
-	//---------------------------------------------------------------------------------------------
 	
 	public double meanThrough(int start, int numSamples) {
-		double m = 0;
-		for(int i=start; i<numSamples; i++)
-			m += ((RealValue)data.elementAt(i)).get();
-		m /= (numSamples-start);
-		return m;
+		double mean = 0;
+		for(int index = start; index < numSamples; index++) {
+			mean += ((RealValue)data.elementAt(index)).get();
+		}			
+		mean /= (numSamples-start);
+		
+		return mean;
 	}
-	
-	//---------------------------------------------------------------------------------------------
-	
-	public static double meanThrough(double[] data, int start, int numSamples) {
-		double m = 0;
-		for(int i=start; i<numSamples; i++)
-			m += data[i];
-		m /= (numSamples-start);
-		return m;
-	}
-	
-	//---------------------------------------------------------------------------------------------
 	
 	public double stdThrough(int start, int numSamples) {
-		double m = meanThrough(0, numSamples);
+		double mean = meanThrough(0, numSamples);
 		double std = 0;
-		for(int i=start; i<numSamples; i++)
-			std += Math.pow(Math.abs(((RealValue)data.elementAt(i)).get() - m), 2);
+		for(int index = start; index < numSamples; index++) {
+			std += Math.pow(Math.abs(((RealValue)data.elementAt(index)).get() - mean), 2);
+		}			
 		std = Math.sqrt(std/((double)(numSamples-start)-1));
+		
 		return std;
 	}
-	
-	//---------------------------------------------------------------------------------------------
-	
-	public static double stdThrough(double[] data, int start, int numSamples) {
-		double m = meanThrough(data, 0, numSamples);
-		double std = 0;
-		for(int i=start; i<numSamples; i++)
-			std += Math.pow(Math.abs(data[i] - m), 2);
-		std = Math.sqrt(std/((double)(numSamples-start)-1));
-		return std;
-	}
-	
-	//---------------------------------------------------------------------------------------------
-	
+
 	public double minThrough(int numSamples) {
-		double min = 99999999, val;
-		for(int i=0; i<numSamples; i++) {
-			val = ((RealValue)data.elementAt(i)).get();
-			if(val < min)
-				min = val;
-		}
-			
-		return min;
+		double minimum = largePositiveNumber;
+		double indexValue;
+		for(int index = 0; index < numSamples; index++) {
+			indexValue = ((RealValue)data.elementAt(index)).get();
+			if(indexValue < minimum)
+				minimum = indexValue;
+		}			
+		return minimum;
 	}
-	
-	//---------------------------------------------------------------------------------------------
 	
 	public double maxThrough(int numSamples) {
-		double min = -99999999, val;
-		for(int i=0; i<numSamples; i++) {
-			val = ((RealValue)data.elementAt(i)).get();
-			if(val > min)
-				min = val;
-		}
-			
-		return min;
+		double maximum = largeNegativeNumber;
+		double indexValue;
+		for(int index = 0; index < numSamples; index++) {
+			indexValue = ((RealValue)data.elementAt(index)).get();
+			if(indexValue > maximum)
+				maximum = indexValue;
+		}			
+		return maximum;
 	}
-	
-	//---------------------------------------------------------------------------------------------
 	
 	public void removeOutliers() {
 		// removes the maximum and minimum values from the data (necessary for one scenario)
 		if(data.elementAt(0) instanceof RealValue) {
-			double min = 99999999, max=-99999999;
-			int minI=-1, maxI=-1;
-			for(int i=0; i<data.size(); i++) {
-				double val = ((RealValue)data.elementAt(i)).get();
-				if(val >= max) {
-					max = val;
-					maxI = i;
+			double min = largePositiveNumber;
+			double max= largeNegativeNumber;
+			int minIndex=-1; 
+			int maxIndex=-1;
+			for(int index = 0; index < data.size(); index++) {
+				double valueIndex = ((RealValue)data.elementAt(index)).get();
+				if(valueIndex >= max) {
+					max = valueIndex;
+					maxIndex = index;
 				}
-				if(val <= min) {
-					min=val;
-					minI=i;
+				if(valueIndex <= min) {
+					min=valueIndex;
+					minIndex=index;
 				}
 			}
-			if(minI > -1)
-				data.removeElementAt( minI );
+			if(minIndex > -1)
+				data.removeElementAt(minIndex);
+				timestamps.removeElementAt(minIndex);
 			// index for maxI will have decreased from the removal
-			if(maxI > 0)
-				data.removeElementAt( maxI-1 );
+			if(maxIndex > 0)
+				data.removeElementAt(maxIndex-1);
+				timestamps.removeElementAt(maxIndex-1);
 		}
 	}
-	
-	//---------------------------------------------------------------------------------------------
 }
